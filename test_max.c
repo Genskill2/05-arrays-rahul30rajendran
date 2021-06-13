5d956d2479b4b5ba300d85d00ec3679c6100612b
rahul30rajendran <84763801+rahul30rajendran@users.noreply.github.com>
2021-06-13 10:51:35 +0530

    soln

--- test_max.c
+++ test_max.c
@@ -12,3 +12,11 @@ int main(void) {
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
   }
+
+int max(int arr[],int n)
+{int max=arr[0];
+for(int i=0;i<n;i++)
+{
+if(max<arr[i])
+max=arr[i];}
+ return max;}
